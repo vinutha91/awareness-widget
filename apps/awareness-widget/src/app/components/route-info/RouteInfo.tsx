4fd1b922b3476b0ed6d7d040a885807a07d69113
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-26 16:21:38 +0530

    Add RouteInfo Component

--- apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
+++ apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
@@ -1,7 +1,66 @@
 import './route-info.scss';
+import { Route } from './route/route';
 
 export const RouteInfo = () =>{
-    return <>
-        Route Info
-    </>
+    return (
+        <section className='routes'>
+            <label className='routes--heading'>Delayed Routes</label>
+            <Route routeDetails={
+                {
+                    id: 123,
+                    name: 'Monash Fwy Out',
+                    from: 'Kings Way',
+                    to: 'EastLink',
+                    distance: '13km',
+                    direction: 'up',
+                    delayed: true,
+                    delayTime: 10,
+                    delayTimeUnit: 'min',
+                    severity: 'low'
+                }
+            }/>
+            <Route routeDetails={
+                {
+                    id: 123,
+                    name: 'Monash Fwy Out',
+                    from: 'Kings Way',
+                    to: 'EastLink',
+                    distance: '13km',
+                    direction: 'up',
+                    delayed: true,
+                    delayTime: 20,
+                    delayTimeUnit: 'min',
+                    severity: 'high'
+                }
+            }/>
+            <Route routeDetails={
+                {
+                    id: 123,
+                    name: 'Western Ring Rd',
+                    from: 'Westgate Fwy',
+                    to: 'Western Fwy',
+                    distance: '20km',
+                    direction: 'down',
+                    delayed: true,
+                    delayTime: 35,
+                    delayTimeUnit: 'min',
+                    severity: 'medium'
+                }
+            }/>
+            <Route routeDetails={
+                {
+                    id: 123,
+                    name: 'Eastern Fwy Out',
+                    from: 'Hoddle St',
+                    to: 'Springvale Rd',
+                    distance: '35km',
+                    direction: 'down',
+                    delayed: true,
+                    delayTime: 15,
+                    delayTimeUnit: 'min',
+                    severity: 'high'
+                }
+            }/>
+        </section>
+    )
 }
