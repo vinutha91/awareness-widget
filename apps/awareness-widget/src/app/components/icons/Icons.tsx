9a07e84b562cabd96b11f9e798ed7579f42a771a
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-26 16:19:01 +0530

    Add icon component

--- apps/awareness-widget/src/app/components/icons/Icons.tsx
+++ apps/awareness-widget/src/app/components/icons/Icons.tsx
@@ -1,5 +1,11 @@
 import { ReactComponent as CloudyDay1Icon } from '../../../assets/icons/weather/cloudy-day-1.svg';
-//apps/awareness-widget/src/assets/icons/weather/cloudy-day-1.svg
+
+import { ReactComponent as UpDirection } from '../../../assets/icons/direction/up.svg';
+import { ReactComponent as DownDirection } from '../../../assets/icons/direction/down.svg';
+
 export const Icons = {
     cloudyDay1: <CloudyDay1Icon/>,
+
+    upDirection: <UpDirection/>,
+    downDirection: <DownDirection/>
 }
