016c10ac9d59d0b16e8f1df901877162f8e6f5ca
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-26 12:25:06 +0530

    Add a sample icon

---
+++ apps/awareness-widget/src/app/components/icons/Icons.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as CloudyDay1Icon } from '../../../assets/icons/weather/cloudy-day-1.svg';
+//apps/awareness-widget/src/assets/icons/weather/cloudy-day-1.svg
+export const Icons = {
+    cloudyDay1: <CloudyDay1Icon/>,
+}
