f23e5b131ea6a5a9995816a3b8a9f95371f99645
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-26 16:17:57 +0530

    Change the import styles

--- apps/awareness-widget/src/app/app.tsx
+++ apps/awareness-widget/src/app/app.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import './app.module.scss';
 import { Dashboard } from './components/dashboard/Dashboard';
 import { RouteInfo } from './components/route-info/RouteInfo';
 import { Weather } from './components/weather/Weather';
