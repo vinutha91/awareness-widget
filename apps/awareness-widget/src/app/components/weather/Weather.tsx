f09aa81b10b4250838bb2ee75b364a6fbd0da37c
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-26 16:21:25 +0530

    Add Weather Component

--- apps/awareness-widget/src/app/components/weather/Weather.tsx
+++ apps/awareness-widget/src/app/components/weather/Weather.tsx
@@ -1,5 +1,43 @@
+import { Icons } from '../icons/Icons';
 import './weather.scss';
 
 export const Weather = () => {
-  return <h1 className="header">hello world</h1>;
+  return (
+    <header className="weather">
+      <section className="weather--summary">
+        <section className="info">
+            <p className='info--city'>
+                Melbourne
+            </p>
+            <p className='info--temperature'>
+                35&deg;
+            </p>
+            <p className='info--date'>
+                Tue 25th Aug, 3:46 PM
+            </p>
+        </section>
+        <section className="icon">
+            {Icons['cloudyDay1']}
+        </section>
+      </section>
+      <section className="weather--details">
+        <section className="detail">
+          <p className='detail--key'>Humidity</p>
+          <p className='detail--value'>50%</p>
+        </section>
+        <section className="detail">
+        <p className='detail--key'>Chance of rain</p>
+          <p className='detail--value'>50%</p>
+        </section>
+        <section className="detail">
+        <p className='detail--key'>Wind</p>
+          <p className='detail--value'>10&nbsp;<span>kmh</span></p>
+        </section>
+        <section className="detail">
+        <p className='detail--key'>Tomorrow</p>
+          <p className='detail--value'>30&deg; {Icons['cloudyDay1']}</p>
+        </section>
+      </section>
+    </header>
+  );
 };
