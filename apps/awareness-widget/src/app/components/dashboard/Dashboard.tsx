b1416d3472372bcfb6d4df79499617e9a3d03602
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 07:21:28 +0530

    Accordion collapse expand in Dashboard

--- apps/awareness-widget/src/app/components/dashboard/Dashboard.tsx
+++ apps/awareness-widget/src/app/components/dashboard/Dashboard.tsx
@@ -1,21 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { DoughnutChart } from './charts/doughnut/Doughnut';
 import './dashboard.scss';
 import { useRampData } from './hooks/useRampData';
+import { Icons } from '../icons/Icons';
 
-export const Dashboard = () =>{
-    const { rampData } = useRampData();
+export const Dashboard = () => {
+  const { rampData } = useRampData();
 
-    useEffect(() =>{
-        console.log(rampData);
-    }, [rampData]);
-    
-    return  <div className='dashboard'>
-      <div className='dashboard--heading'>
-          <label>Ramp Chart</label>
+  const [showDashboard, setShowDashboard] = useState(true);
+
+  const toggleRouteInfo = () => {
+    setShowDashboard(!showDashboard);
+  };
+
+  useEffect(() => {
+    console.log(rampData);
+  }, [rampData]);
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard--heading">
+        <label>Ramp Chart</label>
+        <button
+          className={`skeleton-btn ${!showDashboard ? 'collapsed' : ''}`}
+          onClick={() => toggleRouteInfo()}
+        >
+          {Icons['arrow']}
+        </button>
       </div>
-      <DoughnutChart data={
-        rampData
-      } />
+      {
+        showDashboard && (
+          <DoughnutChart data={rampData} />
+        )
+      }
     </div>
-}
+  );
+};
