6a412b84107c455b8268441424ce8ddeb47402de
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 07:21:38 +0530

    Accordion collapse expand in RouteInfo

--- apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
+++ apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
@@ -1,68 +1,81 @@
+import { useState } from 'react';
+import { Icons } from '../icons/Icons';
 import './route-info.scss';
 import { Route } from './route/route';
 
-export const RouteInfo = () =>{
-    return (
-        <section className='routes'>
-            <div className='routes--heading'>
-                <label>Delayed Routes</label>
-            </div>
-            <Route routeDetails={
-                {
-                    id: 123,
-                    name: 'Monash Fwy Out',
-                    from: 'Kings Way',
-                    to: 'EastLink',
-                    distance: '13km',
-                    direction: 'up',
-                    delayed: true,
-                    delayTime: 10,
-                    delayTimeUnit: 'min',
-                    severity: 'low'
-                }
-            }/>
-            <Route routeDetails={
-                {
-                    id: 123,
-                    name: 'Monash Fwy Out',
-                    from: 'Kings Way',
-                    to: 'EastLink',
-                    distance: '13km',
-                    direction: 'up',
-                    delayed: true,
-                    delayTime: 20,
-                    delayTimeUnit: 'min',
-                    severity: 'high'
-                }
-            }/>
-            <Route routeDetails={
-                {
-                    id: 123,
-                    name: 'Western Ring Rd',
-                    from: 'Westgate Fwy',
-                    to: 'Western Fwy',
-                    distance: '20km',
-                    direction: 'down',
-                    delayed: true,
-                    delayTime: 35,
-                    delayTimeUnit: 'min',
-                    severity: 'medium'
-                }
-            }/>
-            <Route routeDetails={
-                {
-                    id: 123,
-                    name: 'Eastern Fwy Out',
-                    from: 'Hoddle St',
-                    to: 'Springvale Rd',
-                    distance: '35km',
-                    direction: 'down',
-                    delayed: true,
-                    delayTime: 15,
-                    delayTimeUnit: 'min',
-                    severity: 'high'
-                }
-            }/>
-        </section>
-    )
-}
+export const RouteInfo = () => {
+  const [showRouteInfo, setShowRouteInfo] = useState(true);
+
+  const toggleRouteInfo = () => {
+    setShowRouteInfo(!showRouteInfo);
+  };
+
+  return (
+    <section className="routes">
+      <div className="routes--heading">
+        <label>Delayed Routes</label>
+        <button className={`skeleton-btn ${!showRouteInfo ? 'collapsed' : ''}`} onClick={() => toggleRouteInfo()}>{Icons['arrow']}</button>
+      </div>
+      {showRouteInfo && (
+        <>
+          <Route
+            routeDetails={{
+              id: 123,
+              name: 'Monash Fwy Out',
+              from: 'Kings Way',
+              to: 'EastLink',
+              distance: '13km',
+              direction: 'up',
+              delayed: true,
+              delayTime: 10,
+              delayTimeUnit: 'min',
+              severity: 'low',
+            }}
+          />
+          <Route
+            routeDetails={{
+              id: 123,
+              name: 'Monash Fwy Out',
+              from: 'Kings Way',
+              to: 'EastLink',
+              distance: '13km',
+              direction: 'up',
+              delayed: true,
+              delayTime: 20,
+              delayTimeUnit: 'min',
+              severity: 'high',
+            }}
+          />
+          <Route
+            routeDetails={{
+              id: 123,
+              name: 'Western Ring Rd',
+              from: 'Westgate Fwy',
+              to: 'Western Fwy',
+              distance: '20km',
+              direction: 'down',
+              delayed: true,
+              delayTime: 35,
+              delayTimeUnit: 'min',
+              severity: 'medium',
+            }}
+          />
+          <Route
+            routeDetails={{
+              id: 123,
+              name: 'Eastern Fwy Out',
+              from: 'Hoddle St',
+              to: 'Springvale Rd',
+              distance: '35km',
+              direction: 'down',
+              delayed: true,
+              delayTime: 15,
+              delayTimeUnit: 'min',
+              severity: 'high',
+            }}
+          />
+        </>
+      )}
+    </section>
+  );
+};
