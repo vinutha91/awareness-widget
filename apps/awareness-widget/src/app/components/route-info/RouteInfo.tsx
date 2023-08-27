29fbda224b02b2307790117ed630aff2060377bd
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 08:49:00 +0530

    import routes dynamically

--- apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
+++ apps/awareness-widget/src/app/components/route-info/RouteInfo.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Icons } from '../icons/Icons';
 import './route-info.scss';
 import { Route } from './route/route';
+import { ROUTES as routes } from './data/routes';
 
 export const RouteInfo = () => {
   const [showRouteInfo, setShowRouteInfo] = useState(true);
@@ -14,66 +15,18 @@ export const RouteInfo = () => {
     <section className="routes">
       <div className="routes--heading">
         <label>Delayed Routes</label>
-        <button className={`skeleton-btn ${!showRouteInfo ? 'collapsed' : ''}`} onClick={() => toggleRouteInfo()}>{Icons['arrow']}</button>
+        <button
+          className={`skeleton-btn ${!showRouteInfo ? 'collapsed' : ''}`}
+          onClick={() => toggleRouteInfo()}
+        >
+          {Icons['arrow']}
+        </button>
       </div>
       {showRouteInfo && (
         <>
-          <Route
-            routeDetails={{
-              id: 123,
-              name: 'Monash Fwy Out',
-              from: 'Kings Way',
-              to: 'EastLink',
-              distance: '13km',
-              direction: 'up',
-              delayed: true,
-              delayTime: 10,
-              delayTimeUnit: 'min',
-              severity: 'low',
-            }}
-          />
-          <Route
-            routeDetails={{
-              id: 123,
-              name: 'Monash Fwy Out',
-              from: 'Kings Way',
-              to: 'EastLink',
-              distance: '13km',
-              direction: 'up',
-              delayed: true,
-              delayTime: 20,
-              delayTimeUnit: 'min',
-              severity: 'high',
-            }}
-          />
-          <Route
-            routeDetails={{
-              id: 123,
-              name: 'Western Ring Rd',
-              from: 'Westgate Fwy',
-              to: 'Western Fwy',
-              distance: '20km',
-              direction: 'down',
-              delayed: true,
-              delayTime: 35,
-              delayTimeUnit: 'min',
-              severity: 'medium',
-            }}
-          />
-          <Route
-            routeDetails={{
-              id: 123,
-              name: 'Eastern Fwy Out',
-              from: 'Hoddle St',
-              to: 'Springvale Rd',
-              distance: '35km',
-              direction: 'down',
-              delayed: true,
-              delayTime: 15,
-              delayTimeUnit: 'min',
-              severity: 'high',
-            }}
-          />
+          {routes.map((route: any) => {
+            return <Route routeDetails={route} key={route.id} />;
+          })}
         </>
       )}
     </section>
