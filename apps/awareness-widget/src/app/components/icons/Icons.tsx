f1568b595f46c98e301a713407acc5231c92b3bd
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 08:34:49 +0530

    Add icon to registry

--- apps/awareness-widget/src/app/components/icons/Icons.tsx
+++ apps/awareness-widget/src/app/components/icons/Icons.tsx
@@ -1,11 +1,11 @@
-import { ReactComponent as CloudyDay1Icon } from '../../../assets/icons/weather/cloudy-day-1.svg';
+import { ReactComponent as Showers } from '../../../assets/icons/weather/showers.svg';
 
 import { ReactComponent as UpDirection } from '../../../assets/icons/direction/up.svg';
 import { ReactComponent as DownDirection } from '../../../assets/icons/direction/down.svg';
 import { ReactComponent as Arrow } from '../../../assets/icons/accordion/arrow.svg';
 
 export const Icons = {
-    cloudyDay1: <CloudyDay1Icon/>,
+    showers: <Showers/>,
 
     upDirection: <UpDirection/>,
     downDirection: <DownDirection/>,
