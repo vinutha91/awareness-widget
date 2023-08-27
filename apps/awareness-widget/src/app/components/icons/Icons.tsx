ab001281f09ba665220ba38c293bfce6df51bc8d
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 07:21:00 +0530

    Add Arrow Icon

--- apps/awareness-widget/src/app/components/icons/Icons.tsx
+++ apps/awareness-widget/src/app/components/icons/Icons.tsx
@@ -2,10 +2,13 @@ import { ReactComponent as CloudyDay1Icon } from '../../../assets/icons/weather/
 
 import { ReactComponent as UpDirection } from '../../../assets/icons/direction/up.svg';
 import { ReactComponent as DownDirection } from '../../../assets/icons/direction/down.svg';
+import { ReactComponent as Arrow } from '../../../assets/icons/accordion/arrow.svg';
 
 export const Icons = {
     cloudyDay1: <CloudyDay1Icon/>,
 
     upDirection: <UpDirection/>,
-    downDirection: <DownDirection/>
+    downDirection: <DownDirection/>,
+
+    arrow: <Arrow/>
 }
