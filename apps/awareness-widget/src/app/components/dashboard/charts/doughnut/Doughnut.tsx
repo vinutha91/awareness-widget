8b47d184a58844e2b165c7543dedc080ee773d3a
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-08-27 07:30:41 +0530

    Add colors to the doughnut chart

--- apps/awareness-widget/src/app/components/dashboard/charts/doughnut/Doughnut.tsx
+++ apps/awareness-widget/src/app/components/dashboard/charts/doughnut/Doughnut.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import './doughnut.scss'
+import './doughnut.scss';
 
 export const DoughnutChart = ({ data }: any) => {
   const chartRef = useRef(null);
@@ -12,48 +12,52 @@ export const DoughnutChart = ({ data }: any) => {
 
     d3.select(chartRef.current).selectAll('*').remove();
 
-    if (!data)  {
-        return;
+    if (!data) {
+      return;
     }
-    
-    const svg = d3.select(chartRef.current)
+
+    const svg = d3
+      .select(chartRef.current)
       .append('svg')
       .attr('width', width)
       .attr('height', height)
       .append('g')
       .attr('transform', `translate(${width / 2},${height / 2})`);
 
-    const color = d3.scaleOrdinal()
-      .domain(data.map((d: {name: string, number: number}) => d.name))
-      .range(d3.schemeCategory10);
+    const color = d3
+      .scaleOrdinal()
+      .domain(data.map((d: { name: string; number: number }) => d.name))
+      .range(['#26b3aa', '#6fd9d1', '#92e2dd', '#6fd9d1', '#b0e5e0', '#a7dfda', '#bcf2ee','#a1dad5']);
 
-    const pie = d3.pie()
+    const pie = d3
+      .pie()
       .value((d: any) => d.number)
       .sort(null);
 
-    const arc = d3.arc()
+    const arc = d3
+      .arc()
       .innerRadius(radius - 70)
       .outerRadius(radius - 20);
 
-    const arcs = svg.selectAll('arc')
+    const arcs = svg
+      .selectAll('arc')
       .data(pie(data))
       .enter()
       .append('g')
       .attr('class', 'arc');
 
-    arcs.append('path')
+    arcs
+      .append('path')
       .attr('d', arc as any)
       .attr('fill', (d: any) => color(d.data.name) as any);
 
-    arcs.append('text')
-      .attr('transform', d => `translate(${arc.centroid(d as any)})`)
+    arcs
+      .append('text')
+      .attr('transform', (d) => `translate(${arc.centroid(d as any)})`)
       .attr('dy', '.35em')
       .style('text-anchor', 'middle')
       .text((d: any) => d.data.name);
-
   }, [data]);
 
-  return (
-    <div className="chart" ref={chartRef}></div>
-  );
+  return <div className="chart" ref={chartRef}></div>;
 };
